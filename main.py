66ea32c0415bd68ae2120445bb64f38657b734bd
biklajan <127612970+biklajan@users.noreply.github.com>
2023-10-19 20:44:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import discord
 from discord.ext import commands
 import os
@@ -45,15 +44,51 @@ async def heh(ctx, count_heh = 5):
     
 @bot.command()
 async def help(ctx):
-    await ctx.send("Commands: heh, repeat, hello, on_ready, joke")
+    await ctx.send("Commands: heh, henPassword, howSave, degrad(plasick, glass),\nrepeat, hello, on_ready, joke")
     
 @bot.command()
 async def genPassword(ctx, times: int):
     password = ""
     for i in range(times):
         symbol = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        password = password + random.choice(symbol)
+        password = password + random.choice(symbol) 
     await ctx.send(password)
     
 
-bot.run("MTE1NTE2NTU2Njc5ODE0NzcwNQ.GqcJFf.x2JRLNQleJx_mlkoq740CVvAdFHVQAWkFu2fTw")
+@bot.command()
+async def howSave(ctx):
+    await ctx.send(f'1.ЭКОНОМЬТЕ РЕСУРСЫ\n'
+                     '2.РАЗДЕЛЯЙТЕ МУСОР\n'
+                     '3.СДАВАЙТЕ ВТОРСЫРЬЁ\n'
+                     '4.ВЫБИРАЙТЕ ЭКОЛОГИЧНЫЙ ТРАНСПОРТ\n'
+                     '5.ИСПОЛЬЗУЙТЕ ПОВТОРНО И НЕ БЕРИТЕ ЛИШНЕЕ\n'
+                     '6.ВНЕДРЯЙТЕ ЭКО-ПРИВЫЧКИ НА РАБОТЕ\n'
+                     '7.ОБРАТИТЕ ВНИМАНИЕ НА ПИТАНИЕ\n'
+                     '8.ПОСТАРАЙТЕСЬ ОТВЫКНУТЬ ОТ ПЛАСТИКА\n'
+                )
+
+@bot.command()
+async def degrad(ctx, trash):
+    dict = {"plastick": "Сдать пластик можно в «Перекресток»,\n"
+            "«Карусель», «Ашан», «Мега» и др.\n"
+            "\n"
+            "ЧЕМ ОПАСЕН ПЛАСТИК ДЛЯ ОКРУЖАЮЩЕЙ СРЕДЫ?\n"
+            "Основные опасения связаны с тем, что пластмассы, попадая в землю,\n"
+            "распадаются на мелкие частицы и могут выбрасывать в окружающую среду\n"
+            "химические вещества, добавленные в них при производстве.\n"
+            "Это может быть хлор, различные химикаты, например токсичные или канцерогенные антивоспламенители.",
+            
+            "glass": "Стеклянные банки, бутылки можно сдать в экоцентры проектов\n"
+            "«Сборка», «Собиратор» (принимают также стеклянную и хрустальную посуду),\n"
+            "«Переработкинская» (берут и оконные стекла) и т. д.\n"
+            "\n"
+            "На первом месте оказались стеклянные бутылки, которые,\n"
+            "по расчётам исследователей, способствует глобальному\n"
+            "потеплению сильнее, чем пластиковая упаковка и алюминиевые банки.\n"
+            "Как отмечают учёные, для их производства нужно больше ресурсов и энергии,\n"
+            "а в процессе изготовления стекла выделяется много углекислого газа.\n"
+            }
+    await ctx.send(dict[trash])
+    
+
+bot.run("ТВОЙ ЛИЧНЫЙ ТОКЕН")
